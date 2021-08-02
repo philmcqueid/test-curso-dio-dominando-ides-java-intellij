9e11399df0002582cf7b8fa3b478cecd9cc41fd6
Philipe Oliveira <philmcqueid@gmail.com>
2021-08-02 00:41:50 -0300

    criando class Livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -7,6 +7,43 @@ public class PrimeiroPrograma {
         Gato gato = new Gato();
 
         System.out.println(gato);
+        Livro livro = new Livro("O problema do 3 corpos", 300);
 
+        System.out.println(livro);
+    }
+}
+
+
+class Livro {
+    private String name;
+    private Integer numPages;
+
+    public Livro(String name, Integer numPages) {
+        this.name = name;
+        this.numPages = numPages;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getNumPages() {
+        return numPages;
+    }
+
+    public void setNumPages(Integer numPages) {
+        this.numPages = numPages;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "name='" + name + '\'' +
+                ", numPages=" + numPages +
+                '}';
     }
 }
